a65f575c95b100bfc646f4ff1e8b19d8a0b18efb
Ilya Radchenko <ilya@burstcreations.com>
2014-12-04 14:59:10 -0500

    Add 'geometry' method for testing

--- test/index.js
+++ test/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 var test = require('tape');
+var knex = require('knex');
+var lib = require('../lib');
+var db = knex({ client: 'postgres' });
 
-test('ok', function (t) {
+test('has geometry', function (t) {
+  lib(db);
+
+  t.equal(typeof db.geometry, 'function', 'geometry present as function');
   t.end();
 });
